cdda5a601db13107883c4699bab01da00d4e07c6
Ernesto Rodriguez <ernesto.rodriguez@jpl.nasa.gov>
2014-04-24 00:53:01 -0700

    Updated version number to reflect changes

--- src/GDALOGRUtilities/version.py
+++ src/GDALOGRUtilities/version.py
@@ -6,4 +6,8 @@
 # Minor modification to allow a limited version of the package
 # to be imported without requiring shapely.
 
-__version__ = '0.2.1'
+#__version__ = '0.2.1'
+
+# Fixed GDALWriter to accommodate NS or SN row order on input.
+
+__version__ = '0.2.2'
