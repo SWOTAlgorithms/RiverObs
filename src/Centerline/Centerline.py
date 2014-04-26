b91c0af6150cc136846ec30b99efd889efe16aa3
Ernesto Rodriguez <ernesto.rodriguez@jpl.nasa.gov>
2014-04-25 23:07:10 -0700

    Added resampling to center line.

--- src/Centerline/Centerline.py
+++ src/Centerline/Centerline.py
@@ -11,13 +11,17 @@ class Centerline:
     relative to a curved line defined by a series of two dimentional
     points."""
 
-    def __init__(self,x,y,k=3):
+    def __init__(self,x,y,k=3,ds=None):
         """Initialize with a set of x,y points. A Euclidean
-        metric is assumed for distance calculation.
+        metric is assumed for distance calculation. Optionally,
+        the centerline is resampled to a uniform along-track
+        spacing.
 
         x,y: iterables of the same dimension.
         
         k: order of the spline to use for interpolation (1<= k <=5)
+        ds: if not None, resample x,y points to this spacing 
+        (approximately to not extend the interval).
         """
 
         # The following holds the array for cKDTree, which
@@ -25,19 +29,13 @@ class Centerline:
 
         self.x = N.asarray(x)
         self.y = N.asarray(y)
-        self.xy = N.zeros((len(x),2),dtype=N.float64)
-        self.xy[:,0] = x
-        self.xy[:,1] = y
-
-        # Initialize the cKDtree
-
-        self.kdtree = cKDTree(self.xy)
 
         # Compute the distance along the curve
 
         self.delta = N.zeros((len(x),),dtype=N.float64)
-        self.delta[1:] = N.sqrt((self.xy[1:,0]-self.xy[:-1,0])**2 + 
-                                (self.xy[1:,1]-self.xy[:-1,1])**2 )
+        
+        self.delta[1:] = N.sqrt((self.x[1:]-self.x[:-1])**2 + 
+                                (self.y[1:]-self.y[:-1])**2 )
         self.s = N.cumsum(self.delta)
 
         # Compute the spline for evaluating x and y as a function of s
@@ -45,17 +43,37 @@ class Centerline:
         self.xtck = splrep(self.s,self.x,k=k)
         self.ytck = splrep(self.s,self.y,k=k)
 
+        # If resampling is desired, find the new s, x, and y
+
+        if ds != None:
+            ns = int(self.s[-1]/ds + 1)
+            if ns < 2:
+                raise Exception('This ds is too large for the data set')
+
+            self.ds =  self.s[-1]/(ns - 1)
+            self.s = N.arange(ns)*self.ds
+            self.x = splev(self.s,self.xtck)
+            self.y = splev(self.s,self.xtck)
+
+        # Initialize the cKDtree
+
+        self.xy = N.zeros((len(self.x),2),dtype=N.float64)
+        self.xy[:,0] = self.x
+        self.xy[:,1] = self.y
+
+        self.kdtree = cKDTree(self.xy)
+                    
         # Calculate the tangent and normal vectors at each point
 
         self.dx_ds = splev(self.s,self.xtck,der=1)
         self.dy_ds = splev(self.s,self.ytck,der=1)
 
-        self.tangent = N.zeros((len(x),2),dtype=N.float64)
+        self.tangent = N.zeros((len(self.x),2),dtype=N.float64)
         self.tangent[:,0] = self.dx_ds
         self.tangent[:,1] = self.dy_ds
         self.tangent /= N.sqrt(self.tangent[:,0]**2 + self.tangent[:,1]**2)[:,N.newaxis]
 
-        self.normal = N.zeros((len(x),2),dtype=N.float64)
+        self.normal = N.zeros((len(self.x),2),dtype=N.float64)
         self.normal[:,0] = -self.tangent[:,1]
         self.normal[:,1] = self.tangent[:,0]
 
---
+++ src/Centerline/version.py
@@ -0,0 +1,7 @@
+# Version 0.1: basic functionality
+
+#__version__ = '0.1.0'
+
+# Version 0.1.1: added along-track resampling
+
+__version__ = '0.1.1'
