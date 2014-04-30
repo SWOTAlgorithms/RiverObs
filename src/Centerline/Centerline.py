860901a19c9923035269c0d2e6a09a34f9839fc9
Ernesto Rodriguez <ernesto.rodriguez@jpl.nasa.gov>
2014-04-30 10:03:31 -0700

    Added the capability to add observations along the Centerline.

--- src/Centerline/Centerline.py
+++ src/Centerline/Centerline.py
@@ -9,9 +9,14 @@ from scipy.interpolate import splrep, splev
 class Centerline:
     """A class for computing the location of a point or set of points
     relative to a curved line defined by a series of two dimentional
-    points."""
+    points.
 
-    def __init__(self,x,y,k=3,ds=None,eps=1.e-6):
+    As an option, a set of ovservations (e.g., width) can be associated
+    with the center line.
+    """
+
+    def __init__(self,x,y,k=3,ds=None,eps=1.e-6,
+                 obs=None,obs_names=None):
         """Initialize with a set of x,y points. A Euclidean
         metric is assumed for distance calculation. Optionally,
         the centerline is resampled to a uniform along-track
@@ -20,9 +25,18 @@ class Centerline:
         x,y: iterables of the same dimension.
         
         k: order of the spline to use for interpolation (1<= k <=5)
+        
         ds: if not None, resample x,y points to this spacing 
         (approximately to not extend the interval).
+        
         eps: minimum distance for two points to be considered different
+
+        As an option, observations at the centerline locations can be
+        stored and interpolated to the resampled centerline. In this case:
+
+        obs: a list of iterables, each of the same size as x or y
+        obs_names: a list of names, of the same size as obs, that
+                   will be used to set the class members (e.g., ['width'])
         """
 
         # The following holds the array for cKDTree, which
@@ -30,6 +44,16 @@ class Centerline:
 
         self.x = N.asarray(x)
         self.y = N.asarray(y)
+
+        # If observations are provided, store them as class variables
+
+        if obs != None:
+            self.obs_names = obs_names
+            for i,name in enumerate(obs_names):
+                if len(obs[i]) != len(x):
+                    raise Exception('obs size incompatible with x size')
+                
+                exec('self.%s = N.asarray(obs[i])'%name)
 
         # Compute the point separation along the curve
 
@@ -47,6 +71,10 @@ class Centerline:
         self.y = self.y[unique]
         self.delta = self.delta[unique]
 
+        if obs != None:
+            for name in self.obs_names:
+                exec('self.%s = self.%s[unique]'%(name,name))
+
         # Compute the distance along the curve
                 
         self.s = N.cumsum(self.delta)
@@ -55,6 +83,8 @@ class Centerline:
 
         self.xtck = splrep(self.s,self.x,k=k)
         self.ytck = splrep(self.s,self.y,k=k)
+        if obs != None:
+            self.init_obs_tck(self.s,k)
 
         # If resampling is desired, find the new s, x, and y
 
@@ -67,6 +97,9 @@ class Centerline:
             self.s = N.arange(ns)*self.ds
             self.x = splev(self.s,self.xtck)
             self.y = splev(self.s,self.ytck)
+            if obs != None:
+                for name in self.obs_names:
+                    exec('self.%s = splev(self.s,self.obs_tck["%s"])'%(name,name))
 
         # Initialize the cKDtree
 
@@ -116,4 +149,12 @@ class Centerline:
         n = -(x0 - x)*ty + (y0 - y)*tx
 
         return i,d,x,y,s,n
-        
+
+    def init_obs_tck(self,s,k):
+        """Initialize the spline interpolators for the observations."""
+
+        self.obs_tck = {}
+        for name in self.obs_names:
+            exec('x = self.%s'%name)
+            self.obs_tck[name] = splrep(s,x,k=k)
+
