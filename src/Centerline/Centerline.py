db9b3e20ee84a476707b1fd555838edbda381670
Ernesto Rodriguez <ernesto.rodriguez@jpl.nasa.gov>
2014-05-02 09:29:30 -0700

    Added capability for smoothing the spline.

--- src/Centerline/Centerline.py
+++ src/Centerline/Centerline.py
@@ -15,31 +15,64 @@ class Centerline:
     with the center line.
     """
 
-    def __init__(self,x,y,k=3,ds=None,eps=1.e-6,
-                 obs=None,obs_names=None,kobs=1):
+    def __init__(self, x, y, k=3, ds=None, eps=1.e-6,
+                 w=None, s=None,
+                 obs=None, obs_names=None, kobs=1,
+                 wobs=None, sobs=None, **kwds):
         """Initialize with a set of x,y points. A Euclidean
         metric is assumed for distance calculation. Optionally,
         the centerline is resampled to a uniform along-track
         spacing.
 
-        x,y: iterables of the same dimension.
-        
-        k: order of the spline to use for interpolation (1<= k <=5)
-        
-        ds: if not None, resample x,y points to this spacing 
-        (approximately to not extend the interval).
-        
-        eps: minimum distance for two points to be considered different
+        Parameters
+        ----------
+        x,y : iterables of the same dimension.
+        k : int
+            order of the spline to use for interpolation (1<= k <=5)
+        ds : float
+             If not None, resample x,y points to this spacing 
+             (approximately to not extend the interval).
+        eps : float
+              minimum distance for two points to be considered different
+        w : array_like
+            Strictly positive rank-1 array of weights the same length as x and y.
+            The weights are used in computing the weighted least-squares spline
+            fit. If the errors in the y values have standard-deviation given by the
+            vector d, then w should be 1/d. Default is ones(len(x)).
+        s : float
+            A smoothing condition. The amount of smoothness is determined by
+            satisfying the conditions: sum((w * (y - g))**2,axis=0) <= s where g(x)
+            is the smoothed interpolation of (x,y). The user can use s to control
+            the tradeoff between closeness and smoothness of fit. Larger s means
+            more smoothing while smaller values of s indicate less smoothing.
+            Recommended values of s depend on the weights, w. If the weights
+            represent the inverse of the standard-deviation of y, then a good s
+            value should be found in the range (m-sqrt(2*m),m+sqrt(2*m)) where m is
+            the number of datapoints in x, y, and w. default : s=m-sqrt(2*m) if
+            weights are supplied. s = 0.0 (interpolating) if no weights are
+            supplied.            
 
         As an option, observations at the centerline locations can be
         stored and interpolated to the resampled centerline. In this case:
 
-        obs: a list of iterables, each of the same size as x or y
-        
-        obs_names: a list of names, of the same size as obs, that
-                   will be used to set the class members (e.g., ['width'])
+        obs : a list of iterables, each of the same size as x or y
+        obs_names: a list of names, of the same size as obs 
+                   Will be used to set the class members (e.g., ['width'])
+        kobs : int
+               interpolation method for observations (default 1: linear)
+        wobs : array_like
+               Observation weights for splrep.
+        sobs : float
+               Observations smoothing for splrep
+        **kwds:
+               Additional keywords will be passed to splrep.
 
-        kobs: interpolation method for observations (default 1: linear)
+        Notes
+        -----
+
+        Uses scipy.interpolate.splrep (FITPACK). See scipy documentation for
+        further comments on the inputs.
+        
         """
 
         # The following holds the array for cKDTree, which
@@ -84,10 +117,10 @@ class Centerline:
 
         # Compute the spline for evaluating x and y as a function of s
 
-        self.xtck = splrep(self.s,self.x,k=k)
-        self.ytck = splrep(self.s,self.y,k=k)
+        self.xtck = splrep(self.s,self.x,k=k,w=w,s=s,**kwds)
+        self.ytck = splrep(self.s,self.y,k=k,w=w,s=s,**kwds)
         if obs != None:
-            self.init_obs_tck(self.s,kobs)
+            self.init_obs_tck(self.s,k=kobs,w=wobs,s=sobs,**kwds)
 
         # If resampling is desired, find the new s, x, and y
 
@@ -126,20 +159,28 @@ class Centerline:
         self.normal[:,0] = -self.tangent[:,1]
         self.normal[:,1] = self.tangent[:,0]
 
-    def __call__(self,x0,y0):
+    def __call__(self, x0, y0):
         """For each point in (x0,y0), return the nearest point, as well
         as the along and across track coordinates for that point in the
         local coordinate system as the point.
 
-        x0, y0: 1D iterables of the same dimension which can be cast to 
-        numpy 1D arrays.
+        Parameters
+        ----------
+        
+        x0, y0 : array_like
+                 1D iterables of the same dimension which can be cast to
+                 numpy 1D arrays.
 
-        Returns:
+        Returns
+        -------
 
-        index: the index of the nearest point
-        x,y: The coordiantes of the nearest point
-        s,n: The along and across track coordinates of the point
-        relative to the nearest point coordinate system.
+        index : array_like
+                The index of the nearest point
+        x, y : array_like
+               The coordiantes of the nearest point
+        s,n : array_like
+              The along and across track coordinates of the point
+              relative to the nearest point coordinate system.
         """
 
         xy = N.column_stack([x0,y0])
@@ -153,11 +194,11 @@ class Centerline:
 
         return i,d,x,y,s,n
 
-    def init_obs_tck(self,s,k):
+    def init_obs_tck(self,scoord,k=3,s=None,w=None,**kwds):
         """Initialize the spline interpolators for the observations."""
 
         self.obs_tck = {}
         for name in self.obs_names:
             exec('x = self.%s'%name)
-            self.obs_tck[name] = splrep(s,x,k=k)
+            self.obs_tck[name] = splrep(scoord,x,k=k,s=s,w=w,**kwds)
 
