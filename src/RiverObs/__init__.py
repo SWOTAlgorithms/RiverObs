908198fb3ab7ce777e29c668f4f673fc73fadeaa
Ernesto Rodriguez <ernesto.rodriguez@jpl.nasa.gov>
2014-05-05 16:01:06 -0700

    New init and version to reflect code reorg.

---
+++ src/RiverObs/__init__.py
@@ -0,0 +1,10 @@
+"""Associate observations with a river."""
+
+from ReachExtractor import ReachExtractor, RiverReach
+from RiverNode import RiverNode
+from RiverObs import RiverObs
+from FitRiver import FitRiver
+from WidthDataBase import WidthDataBase
+from IteratedRiverObs import IteratedRiverObs
+
+from version import __version__
---
+++ src/RiverObs/version.py
@@ -0,0 +1,7 @@
+# Initial version
+
+#__version__ = '0.1.0'
+
+# Code reorganization
+
+__version__ = '0.1.1'
