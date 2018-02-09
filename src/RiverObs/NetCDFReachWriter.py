76c79d3a9bd8148642088d4f88652d6f999c907c
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2018-02-09 09:08:15 -0800

    store metadata in config file

--- src/RiverObs/NetCDFReachWriter.py
+++ src/RiverObs/NetCDFReachWriter.py
@@ -1,5 +1,19 @@
 import netCDF4
+import os
+import configparser
+import RiverObs
+import configparser
 import numpy as np
+
+NC_METADATA_FILE = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'nc_metadata.cfg')
+
+
+CASTED_METADATA_NAMES = ['_FillValue', 'valid_max', 'valid_min']
+FLOAT_METADATA_NAMES = [
+     'geospatial_lat_max', 'geospatial_lat_min', 'geospatial_lon_max',
+     'geospatial_lon_min']
+INT_METADATA_NAMES = ['NONE',]
 
 def gather_outputs(reach_collection):
     """
@@ -31,6 +45,16 @@ def gather_outputs(reach_collection):
 
     return node_outputs, reach_outputs
 
+def cast_metadata(name, value, base_id):
+    if name in INT_METADATA_NAMES:
+        value = int(value)
+    elif name in FLOAT_METADATA_NAMES:
+        value = float(value)
+    elif name in CASTED_METADATA_NAMES:
+        value = base_id.dtype.type(value)
+
+    return value
+
 def write(nc_file, node_outputs, reach_outputs):
     with netCDF4.Dataset(nc_file, 'w') as ofp:
         ofp.createDimension('node_record', node_outputs['reach_idx'].shape[0])
@@ -45,4 +69,30 @@ def write(nc_file, node_outputs, reach_outputs):
             var = ofp.createVariable(
                 "/reaches/%s" % varname, varvalue.dtype.str, ('reach_record',))
             var[:] = varvalue
+
+
+def fixup_metadata(ncfile):
+    parser = configparser.ConfigParser()
+    parser.read(NC_METADATA_FILE)
+
+    with netCDF4.Dataset(ncfile, 'a') as ifp:
+        for section in parser.keys():
+
+            if section == "DEFAULT":
+                continue
+
+            # globals would go here
+            if section == 'GLOBAL':
+                base_id = ifp
+
+            else:
+                group = section.split('/')[1]
+                varname = section.split('/')[2]
+                base_id = ifp.groups[group][varname]
+
+            for attr_name, attr_value in parser[section].items():
+                print(section, attr_name, attr_value)
+                attr_value = cast_metadata(attr_name, attr_value, base_id)
+                setattr(base_id, attr_name, attr_value)
+
 
