cda99a7c4025c21b77062e5d3a961ca85ab43e0d
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2019-01-24 13:06:06 -0800

    de-junk

--- src/bin/plot_pixcvec.py
+++ src/bin/plot_pixcvec.py
@@ -14,10 +14,6 @@ from SWOTRiver.products.pixcvec import L2PIXCVector
 FIGSIZE = (12, 8)
 DPI = 200
 
-class LLBOX(object):
-    def __init__(self, bbox):
-        self.bounding_box = bbox
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('rivertile', help='rivertile.nc')
@@ -27,17 +23,12 @@ def main():
     rivertile = L2HRRiverTile.from_ncfile(args.rivertile)
     pixc_vector = L2PIXCVector.from_ncfile(args.pixcvec)
 
-
-    reach_ids = rivertile.reaches.reach_id
-
     scatter_colors = ['k', 'r', 'b', 'g']
-
     figure, axis = plt.subplots(figsize=FIGSIZE, dpi=DPI)
     for ii, node_id in enumerate(rivertile.nodes.node_id):
 
-        this_color = scatter_colors[ii%len(scatter_colors)]
-
         mask = pixc_vector.node_index == node_id
+        this_color = scatter_colors[ii%len(scatter_colors)]
         axis.scatter(
             pixc_vector.longitude_vectorproc[mask],
             pixc_vector.latitude_vectorproc[mask],
@@ -50,11 +41,6 @@ def main():
     axis.set_xlabel('longitude')
     axis.set_ylabel('latitude')
     plt.show()
-
-#     figure, axis = plt.subplots(figsize=FIGSIZE, dpi=DPI)
-#     plot = axis.scatter(
-#         data['longitude'], data['latitude'], s=50, c=data[color_field],
-#         edgecolor='none')
 
 if __name__ == "__main__":
     main()
