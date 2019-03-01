4ee3d1e69fb7e9ea657c9b57554b36e0aa462812
Brent A Williams <bawillia@onde.jpl.nasa.gov>
2019-02-28 16:28:48 -0800

    added fancier segmentation algorithm that allows to detach almost disjoint features. Note the centerline approach to picking the segment label closest to the river still puts some disjoint features back into to output.

--- src/bin/preproc_gdem.py
+++ src/bin/preproc_gdem.py
@@ -11,10 +11,123 @@ import logging
 import scipy.stats
 import scipy.ndimage
 import numpy as np
+import scipy.ndimage.morphology
 
 import RiverObs.ReachDatabase
 
 LOGGER = logging.getLogger(__name__)
+
+def wrap_label_for_plots(arr_in, wrap_value=20):
+    # set 0 values to nan and wrap every
+    arr_out=np.zeros(np.shape(arr_in)) + np.nan
+    arr_out[arr_in>0] = arr_in[arr_in>0]
+    return np.mod(arr_out, wrap_value)
+
+def erosion_segmentation(ltype_in, erosion_iter, plot=False):
+    LOGGER.info('erosion_segmentation')
+    # First erode the water mask
+    ltypeb = scipy.ndimage.morphology.binary_erosion(
+        ltype_in,iterations = erosion_iter)
+    ltype = np.zeros_like(ltype_in)
+    ltype[ltypeb] = 1
+
+    # Now segment the image
+    type_label, num_labels = scipy.ndimage.label(ltype)
+    
+    # Assign closest label to orphaned pixels 
+    # (i.e., pixels that were originally eroded out of mask)
+    # first diate out to get back the edges and assign to nearest feature
+    type_label_tmp = type_label.copy()
+    for i in range(erosion_iter):
+        #print("grey_dilation iter:",i)
+        LOGGER.debug(
+                "grey dilation iter: {} of {}".format(i,erosion_iter))
+        tmp1 = scipy.ndimage.morphology.grey_dilation(
+            type_label_tmp,size=5)
+        msk = np.logical_and(np.logical_and(type_label_tmp==0, tmp1>0), ltype_in==1)
+        type_label_tmp[msk] = tmp1[msk]
+    type_label_tmp = type_label_tmp * ltype_in
+    # Now handle remaining orphaned regions 
+    # (e.g., skinny things that completely eroded)
+    orphan_mask = np.zeros_like(ltype_in)
+    orphan_mask[np.logical_and(ltype_in==1, type_label_tmp<=0)] = 1
+    # Dilate the orphan mask to connect regions that touch
+    orphan_dilated = scipy.ndimage.morphology.binary_dilation(
+        orphan_mask,iterations = 1)
+    # segment the orphans
+    orphan_label, num_orphan_labels = scipy.ndimage.label(orphan_dilated)
+    # Combine labels with orphans as highest labels
+    combo_label = type_label_tmp.copy()
+    combo_label[orphan_label>0] = orphan_label[orphan_label>0] + np.max(type_label_tmp)
+    combo_label[ltype_in==0] = 0
+    combo_label0 = combo_label.copy()
+
+    # See if any orphans are actually touching other features
+    # if so, reassign label to those features in the combo_label
+    objects = scipy.ndimage.find_objects(orphan_label)
+    labels = np.unique(orphan_label)
+    for i, slyce in enumerate(objects):
+        label_intersect = type_label_tmp[slyce]*orphan_dilated[slyce]
+        if np.sum(label_intersect)>0:
+            msk = np.logical_and(
+                orphan_label[slyce]==labels[i+1], ltype_in[slyce]==1)
+            combo_label[slyce][msk] = scipy.stats.mode(
+                label_intersect[label_intersect>0])[0]
+    if (False):
+        import matplotlib.pyplot as plt
+        # for setting the zoom
+        a0=7700
+        a1=8200
+        c0=2700
+        c1=3100
+        
+        plt.figure()
+        plt.imshow(wrap_label_for_plots(type_label_tmp),cmap='jet')
+        plt.colorbar()
+        plt.title('type_label_tmp')
+        plt.ylim([a0,a1])
+        plt.xlim(c0,c1)
+
+        plt.figure()
+        plt.imshow(wrap_label_for_plots(type_label),cmap='jet')
+        plt.colorbar()
+        plt.title('type_label')
+        plt.ylim([a0,a1])
+        plt.xlim(c0,c1)
+
+        plt.figure()
+        plt.imshow(ltype_in,cmap='jet')
+        plt.colorbar()
+        plt.title('ltype_in')
+        plt.ylim([a0,a1])
+        plt.xlim(c0,c1)
+
+        
+        plt.figure()
+        plt.imshow(wrap_label_for_plots(combo_label),cmap='jet')
+        plt.colorbar()
+        plt.title('combo_label')
+        plt.ylim([a0,a1])
+        plt.xlim(c0,c1)
+
+       
+        plt.figure()
+        plt.imshow(wrap_label_for_plots(combo_label0),cmap='jet')
+        plt.colorbar()
+        plt.title('combo_label0')
+        plt.ylim([a0,a1])
+        plt.xlim(c0,c1)
+
+       
+        plt.figure()
+        plt.imshow(wrap_label_for_plots(orphan_label),cmap='jet')
+        plt.colorbar()
+        plt.title('orphan_label')
+        plt.ylim([a0,a1])
+        plt.xlim(c0,c1)
+        plt.show()
+
+    return combo_label
 
 def select_river_labels(type, type_label, gdem_x, gdem_y, reaches):
     """
@@ -41,25 +154,31 @@ def select_river_labels(type, type_label, gdem_x, gdem_y, reaches):
             # probably not it if only 1000 pixels in feature
             if cnts[ilabel] < min_compare:
                 continue
-
+            
+            # get the pixels in the feature
             these_x = gdem_x[type_label == uniq_label]
             these_y = gdem_y[type_label == uniq_label]
 
+            # subsample randomly if its big 
             if cnts[ilabel] > max_compare:
                 these_x, these_y = np.random.permutation(
                     np.array([these_x, these_y]))[:, :max_compare]
 
+            # compute distance between the pixels in the feature and the reach
             delta2 = (
                 (these_x[:, np.newaxis] - reach.x)**2 +
                 (these_y[:, np.newaxis] - reach.y)**2)
+
+            # use the min distance as the feature distance
             min_d2 = delta2.min(axis=0)
             type_dist[ilabel] = np.mean(np.sqrt(min_d2))
 
             LOGGER.debug(
-                "reach, label, dist, num: {} {} {} {}".format(
-                    ireach, uniq_label, type_dist[ilabel], len(these_x)))
+                "reach, label, dist, num: {} {} {} {} {}".format(
+                    ireach, uniq_label, type_dist[ilabel], len(these_x), cnts[ilabel]))
 
         this_label = uniq_labels[type_dist.argmin()]
+        print ('this_label: ',this_label)
         if this_label not in labels:
             labels.append(this_label)
 
@@ -74,6 +193,8 @@ def main():
         '-l', '--log-level', type=str, default="warning",
         help="logging level, one of: debug info warning error")
     parser.add_argument('--plot', default=False, action='store_true')
+    parser.add_argument('--erosion-iter', type=int, default=0,
+                        help='erosion iterations')
     args = parser.parse_args()
 
     level = {'debug': logging.DEBUG, 'info': logging.INFO,
@@ -99,8 +220,12 @@ def main():
     # Extract Reaches
     reaches = RiverObs.ReachDatabase.ReachExtractor(args.reachdb_path, llbox)
 
-    # Labeled in descending order of counts
-    type_label, num_labels = scipy.ndimage.label(type)
+    # Optionally erode before segmentation
+    if args.erosion_iter > 0:
+        type_label = erosion_segmentation(type, args.erosion_iter, plot=args.plot)
+    else:
+        # Labeled in descending order of counts
+        type_label, num_labels = scipy.ndimage.label(type)
 
     # Get land and river labels.
     land_label = 0
@@ -124,14 +249,22 @@ def main():
             reach_lat = np.append(reach_lat, item.lat)
 
         figure, axis = plt.subplots()
-        axis.plot(reach_lon, reach_lat, 'rs')
         axis.plot(lon[type==1], lat[type==1], 'g.')
         axis.plot(lon[out_type==1], lat[out_type==1], 'k.')
+        #axis.scatter(lon[type==1], lat[type==1],
+        #             c=wrap_label_for_plots(type_label[type==1]),
+        #             edgecolors='none')
+        axis.plot(reach_lon, reach_lat, 'rs')
         axis.set_xlabel('longitude')
         axis.set_ylabel('latitude')
 
         figure, axis = plt.subplots()
-        axis.imshow(type)
+        axis.imshow(type+out_type)
+        axis.set_title('input+output landtype')
+
+        figure, axis = plt.subplots()
+        axis.imshow(wrap_label_for_plots(type_label))
+        axis.set_title('segmentation label')
 
         plt.show()
 
