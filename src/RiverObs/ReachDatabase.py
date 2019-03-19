976e2fee1cad8fcf76293840dc87ccb6630e325e
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2019-03-19 08:44:37 -0700

    add some debug logging

--- src/RiverObs/ReachDatabase.py
+++ src/RiverObs/ReachDatabase.py
@@ -164,6 +164,8 @@ class ReachExtractor(object):
         self.reach_idx = []
         for ii, reach_idx in enumerate(try_reach_idx):
 
+            LOGGER.debug('Appending reach {} of {}'.format(
+                ii, len(try_reach_idx)))
             this_reach = reach_db(reach_idx)
             lon = this_reach['nodes']['x']
             lat = this_reach['nodes']['y']
