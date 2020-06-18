3964e3749908ba88a7f1586c7939dc9dcef21a9e
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2020-06-18 12:37:22 -0700

    PDD updates

--- src/SWOTRiver/products/riversp.py
+++ src/SWOTRiver/products/riversp.py
@@ -9,13 +9,26 @@ import copy
 import numpy as np
 from collections import OrderedDict as odict
 
+from SWOTWater.products.product import Product, FILL_VALUES, textjoin
 from SWOTRiver.products.rivertile import \
-    L2HRRiverTile, RiverTileNodes, RiverTileReaches, RIVERTILE_ATTRIBUTES
+    L2HRRiverTile, RiverTileNodes, RiverTileReaches, RIVER_PRODUCT_ATTRIBUTES
 
-RIVERSP_ATTRIBUTES = copy.deepcopy(RIVERTILE_ATTRIBUTES)
-RIVERSP_ATTRIBUTES['title'] = {'dtype': 'str', 
-        'value': 'Level 2 KaRIn High Rate River Single Pass Vector Product',
-        'docstr': 'Level 2 KaRIn High Rate River Single Pass Vector Product'}
+RIVERSP_ATTRIBUTES = copy.deepcopy(RIVER_PRODUCT_ATTRIBUTES)
+RIVERSP_ATTRIBUTES['xref_input_l2_hr_rivertile_files'] = {
+    'dtype': 'str',
+    'docstr': 'List of RiverTile products used to generate data in product'}
+RIVERSP_ATTRIBUTES['xref_l2_hr_river_sp_param_file'] = {
+    'dtype': 'str',
+    'docstr': textjoin("""
+        Name of PGE_L2_HR_RiverSP parameter file used to generate data in
+        product""")}
+
+ATTRIBUTE_KEYS2POP = [
+    "_".join([a, b, c]) for a in ['inner', 'outer'] for b in ['first', 'last']
+    for c in ['latitude', 'longitude']] + ['xref_l2_hr_river_tile_param_file',]
+
+for key in ATTRIBUTE_KEYS2POP:
+    RIVERSP_ATTRIBUTES.pop(key, None)
 
 for key in ['Conventions', 'title', 'platform']:
     RIVERSP_ATTRIBUTES[key]['value'] = RIVERSP_ATTRIBUTES[key]['docstr']
@@ -52,9 +65,24 @@ class L2HRRiverSP(L2HRRiverTile):
         klass.reaches = self.reaches + other.reaches
         return klass
 
+    @staticmethod
+    def dump_xmls(node_xml_file, reach_xml_file):
+        with open(node_xml_file, 'w') as ofp:
+            RiverSPNodes.print_xml(ofp=ofp, is_shapefile=True)
+        with open(reach_xml_file, 'w') as ofp:
+            RiverSPReaches.print_xml(ofp=ofp, is_shapefile=True)
+
+
 class RiverSPNodes(RiverTileNodes):
-    ATTRIBUTES = RIVERSP_ATTRIBUTES
+    ATTRIBUTES = RIVERSP_ATTRIBUTES.copy()
+    ATTRIBUTES['title'] = {
+        'dtype': 'str',
+        'value': 'Level 2 KaRIn High Rate River Single Pass Vector Product - Node',
+        'docstr': 'Level 2 KaRIn High Rate River Single Pass Vector Product - Node'}
 
 class RiverSPReaches(RiverTileReaches):
-    ATTRIBUTES = RIVERSP_ATTRIBUTES
-
+    ATTRIBUTES = RIVERSP_ATTRIBUTES.copy()
+    ATTRIBUTES['title'] = {
+        'dtype': 'str',
+        'value': 'Level 2 KaRIn High Rate River Single Pass Vector Product - Reach',
+        'docstr': 'Level 2 KaRIn High Rate River Single Pass Vector Product - Reach'}
