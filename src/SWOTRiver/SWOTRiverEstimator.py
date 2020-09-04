78f0de3040f6b09e128ad6efc1162b87b2eb339a
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2020-09-04 12:17:21 -0700

    use wse_u instead of wse_std for weighted fits

--- src/SWOTRiver/SWOTRiverEstimator.py
+++ src/SWOTRiver/SWOTRiverEstimator.py
@@ -1306,7 +1306,7 @@ class SWOTRiverEstimator(SWOTL2):
         ss = all_ss[self.river_obs.populated_nodes]
 
         hh = river_reach.wse
-        ww = 1/(river_reach.wse_std**2)
+        ww = 1/(river_reach.wse_u**2)
         SS = np.c_[ss, np.ones(len(ss), dtype=ss.dtype)]
 
         mask = np.logical_and(hh > -500, hh < 9000)
