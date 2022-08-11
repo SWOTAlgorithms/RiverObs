f948045ccb7dbb845e8cb6422d0ca79e10fba41f
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2022-08-11 08:38:47 -0700

    add version string (Mmm) to ReachDatabase

--- src/RiverObs/ReachDatabase.py
+++ src/RiverObs/ReachDatabase.py
@@ -320,6 +320,7 @@ class ReachDatabase(Product):
         ['y_max', {'dtype': 'f8' , 'value': None}],
         ['Name', {}], ['production_date', {}],
         ['pass_number', {}], ['tile_number', {}], ['swath_side', {}],
+        ['version', {'dtype': 'str' , 'value': ''}],
         ])
     GROUPS = odict([
         ['nodes', 'ReachDatabaseNodes'],
