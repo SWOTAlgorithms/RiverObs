88d18809ccec28875884d5f2a081e2017d0e064d
Alex Fore <Alexander.Fore@jpl.nasa.gov>
2022-09-12 07:30:53 -0700

    remove ncdiff directory

--- tests/validate_test_case.sh
+++ tests/validate_test_case.sh
@@ -3,6 +3,7 @@
 # Run the python test suite
 pytest -v test_riverobs.py
 
+rm -rf ncdiffs
 mkdir -p ncdiffs
 ./ncdiff_netcdfs.sh reference_case/rt.nc test_case/rt.nc ncdiffs/rt/
 ./ncdiff_netcdfs.sh reference_case/pv.nc test_case/pv.nc ncdiffs/pv/
