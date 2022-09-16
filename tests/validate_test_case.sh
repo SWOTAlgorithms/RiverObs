de6d5c4e8c3acba261f92f50339f21f2ea8d684f
Cassie Stuurman <cassie.stuurman@jpl.nasa.gov>
2022-09-16 10:01:20 -0700

    Clean up console output for missing test case files

--- tests/validate_test_case.sh
+++ tests/validate_test_case.sh
@@ -5,5 +5,10 @@ pytest -v test_riverobs.py
 
 rm -rf ncdiffs
 mkdir -p ncdiffs
-./ncdiff_netcdfs.sh reference_case/rt.nc test_case/rt.nc ncdiffs/rt/
-./ncdiff_netcdfs.sh reference_case/pv.nc test_case/pv.nc ncdiffs/pv/
+
+if test -f "test_case/rt.nc"; then
+  ./ncdiff_netcdfs.sh reference_case/rt.nc test_case/rt.nc ncdiffs/rt/
+  ./ncdiff_netcdfs.sh reference_case/pv.nc test_case/pv.nc ncdiffs/pv/
+else
+  echo "Test case missing. Must run test case before validating it."
+fi
