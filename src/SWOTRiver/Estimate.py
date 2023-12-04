e9b86898ad263d01770d69b6b55f4914c84f4ae7
Alexander Fore <Alexander.Fore@jpl.nasa.gov>
2023-12-04 15:10:30 -0500

    [KCV-344] Fix bug when only 1 populated reach is found (#74)

--- src/SWOTRiver/Estimate.py
+++ src/SWOTRiver/Estimate.py
@@ -288,9 +288,9 @@ class L2PixcToRiverTile(object):
                          self.reach_collection])
 
                 for reach_variable in reach_variables:
-                    self.reach_outputs[reach_variable] = np.squeeze(np.array(
+                    self.reach_outputs[reach_variable] = np.array(
                         [reach.metadata[reach_variable] for reach in
-                         self.reach_collection], dtype=object))
+                         self.reach_collection], dtype=object)
 
                 self.node_outputs['reach_idx'] = np.zeros(
                     self.node_outputs['lat'].shape).astype('int32')
