e28266308e40a6ba43bf61b555d5578013609a07
Cassie Stuurman <Cassie.Stuurman@jpl.nasa.gov>
2025-06-24 10:35:03 -0700

    [KCV-581] Update rdr_sig0 comment from median to mean (#111)
    * Update rdr_sig0 comment from median to mean
    
    * Update rdr_sig0 comment to address negative values
    
    ---------
    
    Co-authored-by: Cassie Stuurman <stuurman@frode.jpl.nasa.gov>

--- src/SWOTRiver/products/rivertile.py
+++ src/SWOTRiver/products/rivertile.py
@@ -1597,11 +1597,12 @@ class RiverTileNodes(ProductTesterMixIn, ShapeWriterMixIn, Product):
                 ['quality_flag', 'node_q'],
                 ['coordinates', 'lon lat'],
                 ['comment', textjoin("""
-                    Median of the sigma0 from the pixel cloud points assigned
-                    to the node in determining the node WSE.  The value is
-                    provided as a linear power ratio, not a value in decibels. 
-                    A decibel value may be obtained from: rdr_sig0_in_dB =
-                    10*log10(rdr_sig0).""")],
+                    Unweighted mean of the sigma0 from the pixel cloud points
+                    assigned to the node in determining the node WSE.  The
+                    value is provided as a linear power ratio, not a value in
+                    decibels. A decibel value may be obtained from:
+                    rdr_sig0_in_dB = 10*log10(rdr_sig0). TThe linear value may
+                    be slightly negative due to noise in the estimate.""")],
                 ])],
         ['rdr_sig0_u',
          odict([['dtype', 'f8'],
